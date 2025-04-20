187ff19803d822b7308c0ffc85e33cd3b4083635
fly19992020 <fly19992020@outlook.com>
2025-04-20 11:07:46 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#[macro_use] extern crate rocket;
+
+use std::fs;
+use rocket::response::content::RawHtml;
+
+#[get("/<name>")]
+fn hello<'r>(name: &str) -> Option<RawHtml<String>> {
+    if name == "Main.html" {
+        let path = "D:/I/my/rust/wikify/src/Main_Page.html";
+        Some(RawHtml(fs::read_to_string(path).unwrap()))
+    }
+    else {
+        Some(RawHtml(String::new()))
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![hello])
+}
