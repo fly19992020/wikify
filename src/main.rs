4ab075bbc963f1491c487cc66a4831333cdb1eee
fly19992020 <fly19992020@outlook.com>
2025-04-20 11:11:08 +0800

    changed the path

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rocket::response::content::RawHtml;
 #[get("/<name>")]
 fn hello<'r>(name: &str) -> Option<RawHtml<String>> {
     if name == "Main.html" {
-        let path = "D:/I/my/rust/wikify/src/Main_Page.html";
+        let path = "./src/Main_Page.html";
         Some(RawHtml(fs::read_to_string(path).unwrap()))
     }
     else {
