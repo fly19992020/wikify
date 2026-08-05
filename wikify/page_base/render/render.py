83bd76cfc85c7902865109be9340aed6a4ad961c
fly19992020 <93079590+fly19992020@users.noreply.github.com>
2026-08-05 22:13:03 +0800

    Improved the render

--- wikify/page_base/render/render.py
+++ wikify/page_base/render/render.py
@@ -1,16 +1,26 @@
 import re
-from typing import List, Callable, Dict
+from typing import List, Callable, Dict, Optional
 
 
 class Render:
     funcs: Dict[str, Callable[[List[str]], str]]
     def __init__(self):
-        self.funcs = {}
-        self.funcs["server"] = (lambda _: "Wikify")
+        self.funcs = {"server": (lambda _: "Wikify")}
 
     def render(self, c: str) -> str:
-        r = re.sub(r"\{[^}]?\}", self.execute, c)
-        return r
+        res = re.sub(r"\{(?P<content>[^}]*)}", self.execute, c)
+        return res
 
-    def execute(self, c: str) -> str:
-        return "Wikify"
+    def execute(self, c: re.Match) -> Optional[str]:
+        c = c.group("content")
+        l = c.split()
+        name = l[0]
+        if name in self.funcs:
+            l.pop(0)
+            return self.funcs[name](l)
+        else:
+            return None
+
+if __name__ == "__main__":
+    r = Render()
+    print(r.render("hello{hello}{server}"))
