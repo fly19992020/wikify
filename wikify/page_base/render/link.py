51b75614c5a2c2f7b5a016c10e08a0eb089c7b70
fly19992020 <93079590+fly19992020@users.noreply.github.com>
2026-08-11 14:18:15 +0800

    Improved the security.

--- wikify/page_base/render/link.py
+++ wikify/page_base/render/link.py
@@ -1,10 +1,15 @@
+import re
 from typing import List
 
 
 def link(l: List[str]) -> str:
     if len(l) == 1:
+        if re.match(r"javascript:.*", l[0]):
+            l[0] = ""
         return "<a href=\"{link}\">{link}</a>".format(link=l[0])
     elif len(l) == 2:
+        if re.match(r"javascript:.*", l[0]):
+            l[0] = ""
         return "<a href=\"{link}\">{name}</a>".format(link=l[0], name=l[1])
     else:
         return ""
